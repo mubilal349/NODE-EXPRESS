ad7ef1485983e3cd5c6e25adc69675c3d6a52460
Muhammad Bilal <95380356+mubilal349@users.noreply.github.com>
2024-01-24 23:57:16 +0500

    Add files via upload
    A little change

--- app.js
+++ app.js
@@ -1,9 +1,21 @@
 const express = require("express"); // require is actually like import a module 
 const app = express();
 
+
+app.use((req,res,next)=>{
+    console.log("MIDDLEWARE");
+    next();
+})
 app.get("/",(req,res)=>{ // get property is used for to get data from server at least 80% used in backend
     res.send("Hello express");// send 
 })
 app.listen(1000);
 
+app.get("/profile",(req,res)=>{
+    res.send("Profile page");
+})
 
+
+app.get("/profile/:names",(req,res)=>{
+    res.send(`hello ${req.params.names}`)
+})
