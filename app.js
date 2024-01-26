4596668480dfbcfd197b702d37da1aea33502f4f
Muhammad Bilal <95380356+mubilal349@users.noreply.github.com>
2024-01-26 23:58:51 +0500

    Add files via upload
    A little changes

--- app.js
+++ app.js
@@ -9,9 +9,17 @@ app.use("",(req,res,next)=>{
     console.log("MIDDLEWARE");
     next();
 })
+app.all("/secret",(req,res,next)=>{
+    res.send("Accessing the secret sec....");
+    next();
+})
 app.get("/", (req, res)=>{
     res.send("welcome to home page");
 });
+
+app.get("/things/:id[0-9]{5}",(req,res)=>{
+    res.send('id:'+req.params.id)
+})
 
 app.get("/contact", (req, res)=>{
     res.send("welcome to contact page");
@@ -20,6 +28,13 @@ app.get("/contact", (req, res)=>{
 app.get("/user/:names", (req, res)=>{
     res.send(`Hi there! I am ${req.params.names}`);
 });
+app.use(function errorHandler(err, req, res, next) {
+    if (res.headersSent) {
+      return next(err);
+    }
+    res.status(500);
+    res.render("error", { error: err });
+  });
 app.get("/portfolio",(req,res)=>{
     res.render("index");
 })
