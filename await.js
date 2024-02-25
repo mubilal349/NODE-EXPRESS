c1e727debbc11ca0b6deb710414d4367e004f541
Muhammad Bilal <95380356+mubilal349@users.noreply.github.com>
2024-02-25 22:35:05 +0500

    AWAIT

---
+++ await.js
@@ -0,0 +1,16 @@
+const getData = async () => {
+	let y = await "Hello World";  //await to wait for the promise to resolve before continuing
+	console.log(y);
+}
+
+console.log(1);
+getData(); // Pause for promise to filter the promise fully
+console.log(2);
+
+let  z = async (e)=>{
+	let $e = await[1,2,3];
+	if
+	console.log($e)
+}
+console.log(1);
+z();
